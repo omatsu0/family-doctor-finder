7e002c5abfe40c81e56bf006ef5f00f0b125cc41
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2021-05-24 04:16:27 +0900

    病院基本登録完成

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -2,4 +2,24 @@ class ClinicsController < ApplicationController
   def show
     @clinic = Clinic.find(params[:id])
   end
+
+  def new
+    @clinic = Clinic.new
+  end
+
+  def create
+    @clinic = Clinic.new(clinic_params)
+    @clinic.user_id = current_user.id
+
+    redirect_to @clinic, notice: '作成しました' if @clinic.save
+  end
+
+  private
+
+  def clinic_params
+    params.require(:clinic).permit(
+      :clinic_name, :clinic_furigana, :clinic_admin_number, :director_name,
+      :phone_number, :introduction, :pdf, :is_pdf_ony, :is_valid
+    )
+  end
 end
