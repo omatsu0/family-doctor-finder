87530a5abb2b751129ad19b5ec20c2ec8489a066
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2021-07-30 02:25:29 +0900

    N+1問題箇所修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,6 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @announses=Announcement.where(user_id: @user.id)
-    @post_clinics = Clinic.where(user_id: @user.id)
+    @post_clinics = Clinic.includes({ location: [:area]} ).where(user_id: @user.id)
   end
 end
